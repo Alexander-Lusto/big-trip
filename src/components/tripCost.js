8f6853b9e86d44e5b40e682379dfc555be6bf00c
Alexander <baks750@mail.ru>
2022-05-12 13:08:15 +0300

    Разбил код на модули

---
+++ src/components/tripCost.js
@@ -0,0 +1,8 @@
+// стоимость поездки
+export const getTripCost = () => {
+  return (`
+    <p class="trip-info__cost">
+      Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+    </p>
+  `);
+};
