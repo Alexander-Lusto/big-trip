7641b8c682839641ff90225cbecaeb7215ca3505
Alexander <baks750@mail.ru>
2022-05-17 13:11:27 +0300

    Создал функцию renderBoard отвечающую за отрисовку дней, точек маршрута и сортировку

--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ import SortingComponent from "./components/sorting";
 import EventEditorComponent from "./components/event-editor";
 import EventComponent from "./components/event";
 import TripDaysComponent from "./components/tripDays";
-import {generatePoints} from "./mock/generatePoints";
+import { generatePoints } from "./mock/generatePoints";
 
 const EVENTS_COUNT = 20;
-const points = generatePoints(EVENTS_COUNT);
+const events = generatePoints(EVENTS_COUNT);
 
 
 // Отрисовка
@@ -17,12 +17,72 @@ const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentElement(place, template);
 };
 
+const renderEvent = (container, point) => {
+  const eventComponent = new EventComponent(point);
+  const event = eventComponent.getElement();
+  const rollupButton = event.querySelector(`.event__rollup-btn`);
+
+  const eventEditorComponent = new EventEditorComponent(point);
+  const eventEditor = eventEditorComponent.getElement();
+  const eventResetButton = eventEditor.querySelector(`.event__reset-btn`);
+  const eventSaveButton = eventEditor.querySelector(`.event__save-btn`);
+
+  const rollupButtonClickHandler = () => {
+    container.replaceChild(eventEditor, event);
+    document.addEventListener(`keydown`, documentEscPressHandler);
+  };
+
+  const eventResetButtonClickHandler = () => {
+    container.replaceChild(event, eventEditor);
+    document.removeEventListener(`keydown`, documentEscPressHandler);
+  };
+
+  const eventSaveButtonClickHandler = () => {
+    container.replaceChild(event, eventEditor);
+    document.removeEventListener(`keydown`, documentEscPressHandler);
+  };
+
+  const documentEscPressHandler = (evt) => {
+    if (evt.key === `Escape` || evt.key === `Esc`) {
+      container.replaceChild(event, eventEditor);
+    }
+  };
+
+  rollupButton.addEventListener(`click`, rollupButtonClickHandler);
+  eventResetButton.addEventListener(`click`, eventResetButtonClickHandler);
+  eventSaveButton.addEventListener(`click`, eventSaveButtonClickHandler);
+
+  render(container, event);
+};
+
+const renderBoard = (container, points) => {
+  const sortingComponent = new SortingComponent();
+  render(container, sortingComponent.getElement());
+
+  const tripDaysComponent = new TripDaysComponent(points);
+  render(container, tripDaysComponent.getElement());
+
+  const eventsList = container.querySelectorAll(`.trip-events__list`);
+
+  let j = 0;
+  for (let i = 0; i < EVENTS_COUNT; i++) {
+    if (i === 0) { // всегда рендерим первый элемент в первый день
+      renderEvent(eventsList[0], points[0]);
+    } else if (points[i].dateFrom.getDate() > points[i - 1].dateFrom.getDate()) { // если (дата элемента > даты предыдущего элемента) рендерим в другой день
+      j++;
+      renderEvent(eventsList[j], points[i]);
+    } else { // иначе рендерим в этот же день
+      renderEvent(eventsList[j], points[i]);
+    }
+  }
+};
+
 const tripMain = document.querySelector(`.trip-main`);
-const tripInfoComponent = new TripInfoComponent(points);
+const tripInfoComponent = new TripInfoComponent(events);
 const tripMainControls = tripMain.querySelector(`.trip-main__trip-controls`);
 render(tripMainControls, tripInfoComponent.getElement(), `beforebegin`);
 
-const tripCostComponent = new TripCostComponent(points);
+const tripCostComponent = new TripCostComponent(events);
 const tripMainInfo = tripMain.querySelector(`.trip-main__trip-info`);
 render(tripMainInfo, tripCostComponent.getElement());
 
@@ -33,27 +93,6 @@ const [tripMainControlsFirstTitle, tripMainControlsSecondTitle] = tripMain.query
 render(tripMainControlsFirstTitle, menuComponent.getElement(), `afterend`);
 render(tripMainControlsSecondTitle, filterComponent.getElement(), `afterend`);
 
-const sortingComponent = new SortingComponent();
 const tripEvents = document.querySelector(`.trip-events`);
-render(tripEvents, sortingComponent.getElement());
 
-const eventEditorComponent = new EventEditorComponent(points[0]);
-render(tripEvents, eventEditorComponent.getElement());
-
-const tripDaysComponent = new TripDaysComponent(points);
-render(tripEvents, tripDaysComponent.getElement());
-
-
-const eventsList = tripEvents.querySelectorAll(`.trip-events__list`);
-
-let j = 0;
-for (let i = 0; i < EVENTS_COUNT; i++) {
-  if (i === 0) { // всегда рендерим первый элемент в первый день
-    render(eventsList[0], new EventComponent(points[0]).getElement());
-  } else if (points[i].dateFrom.getDate() > points[i - 1].dateFrom.getDate()) { // если (дата элемента > даты предыдущего элемента) рендерим в другой день
-    j++;
-    render(eventsList[j], new EventComponent(points[i]).getElement());
-  } else { // иначе рендерим в этот же день
-    render(eventsList[j], new EventComponent(points[i]).getElement());
-  }
-}
+renderBoard(tripEvents, events);
